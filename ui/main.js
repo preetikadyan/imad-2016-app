7c3fea22b7158e0ace4e8388222fcd0fbf5781f3
preetikadyan <preetikadyan634@gmail.com>
2016-10-22 01:36:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var element = document.getElementById("main-text");
 element.innerHTML = "New Value";
+
+//move image
+var element = documnet.getElementById("modi");
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+}
