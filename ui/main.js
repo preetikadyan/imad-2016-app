bc141113e9125bd3709110fd2643c30254a43b57
preetikadyan <preetikadyan634@gmail.com>
2016-10-29 21:41:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,3 +24,17 @@ button.onclick = function(){
     request.send(null);
    
 };
+//submit
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElmentById('submit-btn');
+submit.onclick = function(){
+    
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<=names.length;i++){
+        list += '<li>' + mames[i] + '</li';
+    }
+    var ul = getElementById('namelist');
+    li.innerHTML = list;
+};
